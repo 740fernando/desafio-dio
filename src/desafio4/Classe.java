870857aab5e8d13bdd7b867034abbf050ad52390
Fernando <fernando.luiz92@hotmail.com>
2022-02-19 21:28:26 -0300

    Área do Círculo

--- src/desafio4/Classe.java
+++ src/desafio4/Classe.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 public class Classe {
 
 	public static void main(String[] args) {
+		
 		Scanner scan = new Scanner(System.in);
 
 		Double raio = scan.nextDouble();
 
-		Double area = 3.14159 * (Math.pow( raio   ,2));
+		Double area = 3.14159 * (Math.pow( raio,2));
 
 		System.out.printf("A=%.4f\n", area);
 	}
-
 }
